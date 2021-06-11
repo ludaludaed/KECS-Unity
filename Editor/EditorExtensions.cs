eab6f34aa96390d1d78b263304f789d3dc0260fc
_ludaludaed <43884394+ludaludaed@users.noreply.github.com>
2021-06-11 18:43:46 +0300

    Add files via upload

--- Editor/EditorExtensions.cs
+++ Editor/EditorExtensions.cs
@@ -1,20 +1,40 @@
 ﻿#if UNITY_EDITOR
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 
-namespace Ludaludaed.KECS.Unity.Editor
+namespace Ludaludaed.KECS.Unity
 {
+    public interface IComponent
+    {
+    }
     internal static class EditorExtensions
     {
         private static readonly MethodInfo _setComponentMethod;
         private static readonly MethodInfo _removeComponentMethod;
         private static readonly MethodInfo _hasComponentMethod;
+        private static readonly MethodInfo _regComponentMethod;
 
         static EditorExtensions()
         {
             _setComponentMethod = typeof(EditorExtensions).GetMethod("SetComponent");
             _removeComponentMethod = typeof(EditorExtensions).GetMethod("RemoveComponent");
             _hasComponentMethod = typeof(EditorExtensions).GetMethod("HasComponent");
+            _regComponentMethod = typeof(EditorExtensions).GetMethod("Registration");
+            Init();
+        }
+
+        private static void Init()
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (!typeof(IComponent).IsAssignableFrom(type) || type.IsInterface) continue;
+                    var regComponentGeneric = _regComponentMethod.MakeGenericMethod(type);
+                    regComponentGeneric.Invoke(null,null);
+                }
+            }
         }
 
         internal static void SetExecute(this in Entity entity, object component)
@@ -48,6 +68,11 @@ namespace Ludaludaed.KECS.Unity.Editor
         public static bool HasComponent<T>(this Entity entity) where T : struct
         {
             return entity.Has<T>();
+        }
+        
+        public static void Registration<T>() where T : struct
+        {
+            var idx = ComponentTypeInfo<T>.TypeIndex;
         }
     }
 }
