347648e899deb678a5b3721844dce23cc4a0419b
_ludaludaed <43884394+ludaludaed@users.noreply.github.com>
2022-01-05 15:37:31 +0300

    Add files via upload

--- Editor/Drawers/TypeDrawers.cs
+++ Editor/Drawers/TypeDrawers.cs
@@ -29,15 +29,6 @@ namespace Ludaludaed.KECS.Unity.Editor {
         protected abstract bool OnTypedDraw(string label, ref T value);
     }
 
-    public sealed class AnimationCurveTypeDrawer : TypeDrawer<AnimationCurve> {
-        protected override bool OnTypedDraw(string label, ref AnimationCurve value) {
-            var newValue = EditorGUILayout.CurveField(label, value);
-            if (newValue.Equals(value)) return false;
-            value = newValue;
-            return true;
-        }
-    }
-
     public sealed class BoolTypeDrawer : TypeDrawer<bool> {
         protected override bool OnTypedDraw(string label, ref bool value) {
             var newValue = EditorGUILayout.Toggle(label, value);
@@ -47,27 +38,27 @@ namespace Ludaludaed.KECS.Unity.Editor {
         }
     }
 
-    public sealed class BoundsTypeDrawer : TypeDrawer<Bounds> {
-        protected override bool OnTypedDraw(string label, ref Bounds value) {
-            var newValue = EditorGUILayout.BoundsField(label, value);
-            if (newValue.Equals(value)) return false;
-            value = newValue;
-            return true;
-        }
-    }
-
-    public sealed class ColorTypeDrawer : TypeDrawer<Color> {
-        protected override bool OnTypedDraw(string label, ref Color value) {
-            var newValue = EditorGUILayout.ColorField(label, value);
-            if (newValue.Equals(value)) return false;
-            value = newValue;
-            return true;
-        }
-    }
-
     public sealed class FloatTypeDrawer : TypeDrawer<float> {
         protected override bool OnTypedDraw(string label, ref float value) {
             var newValue = EditorGUILayout.FloatField(label, value);
+            if (Math.Abs(newValue - value) < float.Epsilon) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class DoubleTypeDrawer : TypeDrawer<double> {
+        protected override bool OnTypedDraw(string label, ref double value) {
+            var newValue = EditorGUILayout.DoubleField(label, value);
+            if (Math.Abs(newValue - value) < double.Epsilon) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class LongTypeDrawer : TypeDrawer<long> {
+        protected override bool OnTypedDraw(string label, ref long value) {
+            var newValue = EditorGUILayout.LongField(label, value);
             if (newValue.Equals(value)) return false;
             value = newValue;
             return true;
@@ -92,6 +83,51 @@ namespace Ludaludaed.KECS.Unity.Editor {
         }
     }
 
+    public sealed class AnimationCurveTypeDrawer : TypeDrawer<AnimationCurve> {
+        protected override bool OnTypedDraw(string label, ref AnimationCurve value) {
+            var newValue = EditorGUILayout.CurveField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class BoundsTypeDrawer : TypeDrawer<Bounds> {
+        protected override bool OnTypedDraw(string label, ref Bounds value) {
+            var newValue = EditorGUILayout.BoundsField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class ColorTypeDrawer : TypeDrawer<Color> {
+        protected override bool OnTypedDraw(string label, ref Color value) {
+            var newValue = EditorGUILayout.ColorField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class GradientTypeDrawer : TypeDrawer<Gradient> {
+        protected override bool OnTypedDraw(string label, ref Gradient value) {
+            var newValue = EditorGUILayout.GradientField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class LayerMaskTypeDrawer : TypeDrawer<LayerMask> {
+        protected override bool OnTypedDraw(string label, ref LayerMask value) {
+            var newValue = EditorGUILayout.LayerField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
     public sealed class Vector2Drawer : TypeDrawer<Vector2> {
         protected override bool OnTypedDraw(string label, ref Vector2 value) {
             var newValue = EditorGUILayout.Vector2Field(label, value);
@@ -101,9 +137,36 @@ namespace Ludaludaed.KECS.Unity.Editor {
         }
     }
 
+    public sealed class Vector2IntDrawer : TypeDrawer<Vector2Int> {
+        protected override bool OnTypedDraw(string label, ref Vector2Int value) {
+            var newValue = EditorGUILayout.Vector2IntField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
     public sealed class Vector3Drawer : TypeDrawer<Vector3> {
         protected override bool OnTypedDraw(string label, ref Vector3 value) {
             var newValue = EditorGUILayout.Vector3Field(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class Vector3IntDrawer : TypeDrawer<Vector3Int> {
+        protected override bool OnTypedDraw(string label, ref Vector3Int value) {
+            var newValue = EditorGUILayout.Vector3IntField(label, value);
+            if (newValue.Equals(value)) return false;
+            value = newValue;
+            return true;
+        }
+    }
+
+    public sealed class Vector4Drawer : TypeDrawer<Vector4> {
+        protected override bool OnTypedDraw(string label, ref Vector4 value) {
+            var newValue = EditorGUILayout.Vector4Field(label, value);
             if (newValue.Equals(value)) return false;
             value = newValue;
             return true;
